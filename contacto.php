b9e1dfa3a67660c417c0a070debabafc32ffc248
gabi-devel <gabrielabustos07@gmail.com>
2025-04-18 14:43:33 -0300

    modificado contacto.php

--- contacto.php
+++ contacto.php
@@ -19,8 +19,8 @@ $size_our_products = "60";
                                         <i class="icon-telephone-call-1"></i>
                                     </div>
                                     <div class="contact-one__info__content">
-                                        <p class="contact-one__info__text">Have any question?</p>
-                                        <h4 class="contact-one__info__title"><a href="tel:+92(8800)-8960">Free + 23 (000)-8050</a></h4><!-- /.contact-one__info__title -->
+                                        <p class="contact-one__info__text">Teléfono</p>
+                                        <h4 class="contact-one__info__title"><a href="tel:++54 11 5235-3970">+54 11 5235-3970</a></h4><!-- /.contact-one__info__title -->
                                     </div><!-- /.contact-one__info__content -->
                                 </li>
                                 <li class="contact-one__info__item">
@@ -28,8 +28,8 @@ $size_our_products = "60";
                                         <i class="icon-email"></i>
                                     </div>
                                     <div class="contact-one__info__content">
-                                        <p class="contact-one__info__text">Send email</p>
-                                        <h4 class="contact-one__info__title"><a href="mailto:needhelp@company.com">demo@gmail.com</a></h4>
+                                        <p class="contact-one__info__text">Email</p>
+                                        <h4 class="contact-one__info__title"><a href="mailto:ventas@onyva.com.ar">ventas@onyva.com.ar</a></h4>
                                     </div>
                                 </li>
                                 <li class="contact-one__info__item">
@@ -37,8 +37,8 @@ $size_our_products = "60";
                                         <i class="icon-pin"></i>
                                     </div><!-- /.contact-one__info__icon -->
                                     <div class="contact-one__info__content">
-                                        <p class="contact-one__info__text">Visit anytime</p> <!-- /.contact-one__info__text -->
-                                        <h4 class="contact-one__info__title"><a href="#">86391 elgin st. delaware</a></h4><!-- /.contact-one__info__title -->
+                                        <p class="contact-one__info__text">Visitanos</p> <!-- /.contact-one__info__text -->
+                                        <h4 class="contact-one__info__title"><a href="#">Dr. Adolfo Dickman 990</a></h4><!-- /.contact-one__info__title -->
                                     </div><!-- /.contact-one__info__content -->
                                 </li>
                             </ul><!-- /.list-unstyled -->
@@ -59,29 +59,40 @@ $size_our_products = "60";
                         <div class="contact-one__left">
                             <div class="contact-one__top">
                                 <div class="sec-title wow fadeInUp animated" data-wow-duration="700ms" style="visibility: visible; animation-duration: 700ms; animation-name: fadeInUp;">
-                                    <h6 class="sec-title__tagline"><img src="assets/images/shapes/sec-title-s-1.png" alt="Contact with  Us" class="sec-title__img">Contact with Us</h6><!-- /.sec-title__tagline -->
-                                    <h3 class="sec-title__title">Feel free to write us anytime</h3><!-- /.sec-title__title -->
+                                    <h6 class="sec-title__tagline"><img src="assets/images/shapes/sec-title-s-1.png" alt="Contact with  Us" class="sec-title__img">Contactanos</h6><!-- /.sec-title__tagline -->
+                                    <h3 class="sec-title__title">Complete este breve formulario y nos pondremos en contacto con Ud. a la brevedad</h3><!-- /.sec-title__title -->
                                 </div><!-- /.sec-title -->
                             </div><!-- /.contact-one__top -->
                             <form class="contact-one__form contact-form-validated form-one background-base wow fadeInUp animated" data-wow-duration="1500ms" action="inc/sendemail.php" novalidate="novalidate" style="visibility: visible; animation-duration: 1500ms; animation-name: fadeInUp;">
                                 <div class="form-one__group">
                                     <div class="form-one__control">
-                                        <input type="text" name="name" placeholder="Your name">
+                                        <input placeholder="Ingrese su nombre" name="name" type="text" id="name" class="form-control" value="">
                                     </div><!-- /.form-one__control form-one__control__full -->
+
                                     <div class="form-one__control">
-                                        <input type="email" name="email" placeholder="Email address">
+                                    <input placeholder="Empresa o Laboratorio" name="company" type="text" id="company" class="form-control" value="">
                                     </div><!-- /.form-one__control form-one__control__full -->
-                                    <div class="form-one__control form-one__control">
-                                        <input type="tel" name="form_phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone" required="" aria-required="true">
-                                    </div><!-- /.form-one__control form-one__control__full -->
-                                    <div class="form-one__control form-one__control">
-                                        <input type="text" name="subject" placeholder="Subject">
-                                    </div><!-- /.form-one__control form-one__control__full -->
+                                    
+                                    <div class="form-one__control">
+                                        <input placeholder="Ingrese su email" name="email" type="email" id="email" class="form-control" value="">
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+                                    <div class="form-one__control">
+                                        <input placeholder="Reingrese su email" name="confirmEmail" type="email" id="confirmEmail" class="form-control" value="">
+                                        <div class="invalid-feedback"></div>
+                                    </div>
+
                                     <div class="form-one__control form-one__control--full">
-                                        <textarea name="message" placeholder="Write a message"></textarea><!-- /# -->
+                                        <textarea name="message" placeholder="Escriba su mensaje"></textarea><!-- /# -->
                                     </div><!-- /.form-one__control -->
                                     <div class="form-one__control form-one__control--full">
-                                        <button type="submit" class="laboix-btn laboix-btn--submite">Send a message</button>
+                                        <div class="form-check">
+                                            <input name="terms" type="checkbox" class="form-check-input">
+                                            <p title="" class="form-check-label">Acepto recibir información sobre productos y servicios</p>
+                                        </div>
+                                    </div><!-- /.form-one__control -->
+                                    <div class="form-one__control form-one__control--full">
+                                        <button type="submit" class="laboix-btn laboix-btn--submite">Enviar</button>
                                     </div><!-- /.form-one__control -->
                                 </div><!-- /.form-one__group -->
                             </form>
